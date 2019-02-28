087485dff6c0ed447175bfbb140ceefd81ce2d5f
Ikra Rashid <irashid@eg.poly.edu>
2019-02-28 15:43:34 -0500

    added a .gitignore file where node modules and config.js would be stored

--- twitBot_Cool/config.js
+++ twitBot_Cool/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  consumer_key:         '',
-  consumer_secret:      '',
-  access_token:         '',
-  access_token_secret:  '',
+  consumer_key:         'oxaC4tPMg6Co0Z2qr98YPUD8v',
+  consumer_secret:      '601AtYYAywc8YbVE0FwR2gMmvWddjHc8T1nGumytPIHfTlA36j',
+  access_token:         '1100372886743908352-Uc4sQkCPqEoieKk7LcqKsgylwC5drR',
+  access_token_secret:  'BzKYM9EIvq1KTlXDMR3tYnYagwgFiHKqjeMMbg1mu5xH9',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true     // optional - requires SSL certificates to be valid.
 }
