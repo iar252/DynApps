f29b9b8dda7e7f4bac683a4a650f37032952a9b3
iar252 <iar252@nyu.edu>
2019-02-28 15:20:16 -0500

    Update config.js

--- twitBot_Cool/config.js
+++ twitBot_Cool/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  consumer_key:         'mpzWTCOKTtXmKe9tRPEHZI76D',
-  consumer_secret:      'FIgejr8vHnhRunUzdsC1e3pp0r1rj4VA4G80le46dCx24sriug',
-  access_token:         '1100372886743908352-lZgyx64ct9vVz1OJ5DIOJo7rDgrqo4',
-  access_token_secret:  'eqWRSCKyAjd195gmAqoAtx8iUjHZP7XDFn8HBtTPrU4pa',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true     // optional - requires SSL certificates to be valid.
 }
