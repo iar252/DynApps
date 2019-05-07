e57f57be85f5266eb5431b2f90e31716ab9154dc
Ikra Rashid <irashid@eg.poly.edu>
2019-05-07 16:46:54 -0400

    final project for dynapps

---
+++ polaroidMemories /README.md
@@ -0,0 +1,18 @@
+# Polaroid Memories 
+
+## About
+This is an application where people can select photos to create into polaroids to be able to send to someone. The way it works is that the user can navigate to the page where they upload a photo, it will reflect on the next page as a polaroid, and they can then select more photos. These photos are uploaded onto the server folder, under "uploads". The code changes the names of the files as they are entered. After four photos, the user can begin writing over old ones. 
+
+##How to Use
+*npm install the following:
+*express 
+*hbs 
+*connect-multiparty 
+*request 
+*body-parser
+
+##Future Improvements
+*Users will be able to drop the photos directly into the polaroids then click a submit button which will generate the final page with the polaroids 
+*Users can incorporate videos
+*Users can choose the font and colors of the page 
+*Users will be able to choose how many polaroids they want displayed 
---
+++ polaroidMemories /app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var app = express();
+var hbs = require('hbs');
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart({uploadDir: './uploads/'});
+var request = require('request');
+var fs = require('fs');
+//const multer = require('multer');
+//const upload = multer({dest: './uploads'})
+//const logger = require('morgan');
+const bodyParser = require('body-parser')
+var submitted = false;
+var count = 0;
+
+app.use(express.static('public'));
+app.use(express.static('uploads'));
+app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+
+app.get('/', function(req,res){
+	res.render('home');
+})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.post('/', function(req,res){
+	submitted = true;
+	if(submitted){
+		res.redirect('template');
+	}
+})
+
+app.get('/template', function(req,res){
+	res.render('template');
+})
+
+app.post('/template', function(req,res){
+	res.redirect('submitPic')
+})
+
+app.get('/submitPic', function(req, res){
+	console.log('in submit')
+	res.render('submitPic');
+})
+
+app.post('/submitPic',multipartMiddleware, (req,res)=>{
+	const fileName = req.files.myFile.originalFilename;
+	console.log('filename: ', fileName);
+	const oldFilePath = req.files.myFile.path.split("/");
+	console.log(oldFilePath);
+	
+	if(count != 3){
+		count += 1
+		oldFilePath[1] = 'file' + count + '.png';
+		const newFilePath = oldFilePath.join("/")
+		fs.rename(req.files.myFile.path, newFilePath, function(err){
+			if(err) throw err;
+		})
+	}
+	else{
+		count = 0;
+	}
+})
+
+
+app.listen(3000,function(){
+	console.log("listening on port 3000");
+})
